a059eaa93f0abe7692cada7242b787876ff6b501
Mauro Iazzi <mauro.iazzi@gmail.com>
2007-09-15 18:07:06 +0200

    changed include in function generator. compacted writes in header and definition files. removed debug output.

--- generate_function.lua
+++ generate_function.lua
@@ -20,66 +20,26 @@ hpp = {
 
 hpp.write("#ifndef "..HEADER_DEFINE.."\n")
 hpp.write("#define "..HEADER_DEFINE.."\n")
-cpp.write('#include "luafunction.hpp"\n ')
+cpp.write('#include "lqt_function.hpp"\n ')
 
 
 hpp.write[[
 
-#include "common_bind.hpp"
+#include "lqt_common.hpp"
 
 #include <QObject>
-#include <QDebug>
+//#include <QDebug>
 
-#define LUA_FUNCTION_REGISTRY "LuaFunction"
-#ifndef SEE_STACK
-# define SEE_STACK(L, j) for (int j=1;j<=lua_gettop(L);j++) { qDebug() << j << '=' << luaL_typename(L, j) << '@' << lua_topointer (L, j); }
-#endif
+#define LUA_FUNCTION_REGISTRY "Registry Function"
+//#ifndef SEE_STACK
+//# define SEE_STACK(L, j) for (int j=1;j<=lua_gettop(L);j++) { qDebug() << j << '=' << luaL_typename(L, j) << '@' << lua_topointer (L, j); }
+//#endif
 
 class LuaFunction: public QObject {
   Q_OBJECT
 
   public:
   LuaFunction(lua_State *state);
-]]
-cpp.write[[
-LuaFunction::LuaFunction(lua_State *state):L(state) {
-    int functionTable = lua_gettop(L); // not yet but soon
-    qDebug() << "Function" << this << "is born";
-    lua_getfield(L, LUA_REGISTRYINDEX, LUA_FUNCTION_REGISTRY);
-    if (lua_isnil(L, -1)) {
-      lua_pop(L, 1);
-      lua_newtable(L);
-      lua_pushvalue(L, -1);
-      lua_setfield(L, LUA_REGISTRYINDEX, LUA_FUNCTION_REGISTRY);
-    }
-    lua_insert(L, -2);
-
-    lua_pushvalue(L, -1);
-    lua_gettable(L, functionTable);
-
-    if (!lqtL_testudata(L, -1, "QObject*")) {
-      qDebug() << "not QObject* is" << luaL_typename(L, -1);
-      lua_pop(L, 1);
-      // top of stack is the function I want
-      qDebug() << "to be bound is" << luaL_typename(L, -1);
-      lua_pushlightuserdata(L, this);
-      lua_pushvalue(L, -2);
-      lua_settable(L, functionTable);
-      // registry this is associated to this function
-      lqtL_passudata(L, this, "QObject*");
-      lua_insert(L, -2);
-      lua_pushvalue(L, -2);
-      lua_settable(L, functionTable);
-    } else {
-      // leave the qobject on top;
-      qDebug() << "Function" << this << "scheduled for deletion";
-      this->deleteLater();
-    }
-    lua_replace(L, functionTable);
-    lua_settop(L, functionTable);
-  }
-]]
-hpp.write[[
   virtual ~LuaFunction();
 
   private:
@@ -90,8 +50,53 @@ hpp.write[[
   public slots:
 ]]
 cpp.write[[
+LuaFunction::LuaFunction(lua_State *state):L(state) {
+	int functionTable = lua_gettop(L); // not yet but soon
+	//qDebug() << "Function" << this << "is born";
+	lua_getfield(L, LUA_REGISTRYINDEX, LUA_FUNCTION_REGISTRY);
+	if (lua_isnil(L, -1)) {
+		lua_pop(L, 1);
+		lua_newtable(L);
+		lua_pushvalue(L, -1);
+		lua_setfield(L, LUA_REGISTRYINDEX, LUA_FUNCTION_REGISTRY);
+	}
+	lua_insert(L, -2);
+
+	lua_pushvalue(L, -1);
+	lua_gettable(L, functionTable);
+
+	if (!lqtL_testudata(L, -1, "QObject*")) {
+		//qDebug() << "not QObject* is" << luaL_typename(L, -1);
+		lua_pop(L, 1);
+		// top of stack is the function I want
+		//qDebug() << "to be bound is" << luaL_typename(L, -1);
+		lua_pushlightuserdata(L, this);
+		lua_pushvalue(L, -2);
+		lua_settable(L, functionTable);
+		// registry this is associated to this function
+		lqtL_passudata(L, this, "QObject*");
+		lua_insert(L, -2);
+		lua_pushvalue(L, -2);
+		lua_settable(L, functionTable);
+	} else {
+		// leave the qobject on top;
+		//qDebug() << "Function" << this << "scheduled for deletion";
+		this->deleteLater();
+	}
+	lua_replace(L, functionTable);
+	lua_settop(L, functionTable);
+}
 LuaFunction::~LuaFunction() {
-  qDebug() << "Function" << this << "is dead";
+	//qDebug() << "Function" << this << "is dead";
+	lua_getfield(L, LUA_REGISTRYINDEX, LUA_FUNCTION_REGISTRY);
+	lua_pushlightuserdata(L, this);
+	lua_gettable(L, -2);
+	lua_pushnil(L);
+	lua_settable(L, -3);
+	lua_pushlightuserdata(L, this);
+	lua_pushnil(L);
+	lua_settable(L, -3);
+	lua_pop(L, 1);
 }
 ]]
 --[[
@@ -150,9 +155,8 @@ hpp.write[[
 
 
 ]]
-
 hpp.write("#endif // "..HEADER_DEFINE.."\n")
 
 
-io.write(cpp.string)
+io.write(hpp.string)
 
