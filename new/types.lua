f863ebfaec3de24d8905331ecfefa7b8d0064131
Mauro Iazzi <mauro.iazzi@gmail.com>
2008-03-13 16:20:28 +0100

    more desriptive information on basic types

--- new/types.lua
+++ new/types.lua
@@ -1,39 +1,47 @@
+local BaseType = function(s)
+				return {
+								label = "BaseType",
+								xargs = {  },
+								on_stack = s..';',
+				}
+end
 
-return {
+local base_types = {
 				--['void'] =   function(i) return '(void)(L, ' .. tostring(i) .. ')' end,
 				--['void*'] =   function(i) return 'lua_pushlightuserdata(L, ' .. tostring(i) .. ')' end,
 				--['void**'] = function(i) return 'lua_pushlightuserdata(L, ' .. tostring(i) .. ')' end,
 				--['void const*'] =   function(i) return 'lua_pushlightuserdata(L, ' .. tostring(i) .. ')' end,
 				--['void const**'] = function(i) return 'lua_pushlightuserdata(L, ' .. tostring(i) .. ')' end,
 
-				['char*'] = 'string;',
+				['char*'] = BaseType'string',
 				--['char**'] = function(i) return 'lqtL_pusharguments(L, ' .. tostring(i) .. ')' end,
-				['char const*'] = 'string;',
+				['char const*'] = BaseType'string',
 				--['char const**'] = function(i) return 'lqtL_pusharguments(L, ' .. tostring(i) .. ')' end,
 
-				['int'] =                    'integer;',
-				['unsigned int'] =           'integer;',
+				['int'] =                    BaseType'integer',
+				['unsigned int'] =           BaseType'integer',
 
-				['short'] =                  'integer;',
-				['short int'] =              'integer;',
-				['unsigned short'] =         'integer;',
-				['unsigned short int'] =     'integer;',
-				['short unsigned int'] =     'integer;',
+				['short'] =                  BaseType'integer',
+				['short int'] =              BaseType'integer',
+				['unsigned short'] =         BaseType'integer',
+				['unsigned short int'] =     BaseType'integer',
+				['short unsigned int'] =     BaseType'integer',
 
-				['long'] =                   'integer;',
-				['unsigned long'] =          'integer;',
-				['long int'] =               'integer;',
-				['unsigned long int'] =      'integer;',
-				['long unsigned int'] =      'integer;',
+				['long'] =                   BaseType'integer',
+				['unsigned long'] =          BaseType'integer',
+				['long int'] =               BaseType'integer',
+				['unsigned long int'] =      BaseType'integer',
+				['long unsigned int'] =      BaseType'integer',
 
-				['long long'] =              'integer;',
-				['unsigned long long'] =     'integer;',
-				['long long int'] =          'integer;',
-				['unsigned long long int'] = 'integer;',
+				['long long'] =              BaseType'integer',
+				['unsigned long long'] =     BaseType'integer',
+				['long long int'] =          BaseType'integer',
+				['unsigned long long int'] = BaseType'integer',
 
-				['float'] =  'number;',
-				['double'] = 'number;',
+				['float'] =  BaseType'number',
+				['double'] = BaseType'number',
 
-				['bool'] = 'bool;',
+				['bool'] = BaseType'bool',
 }
 
+return base_types
