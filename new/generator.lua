41c053f07c7eda5b60338e5848538c8a826c23b3
Mauro Iazzi <mauro.iazzi@gmail.com>
2008-03-18 01:00:10 +0100

    removed a lot of rather old code

--- new/generator.lua
+++ new/generator.lua
@@ -13,6 +13,11 @@ local code = xmlstream[1]
 local decompound = function(n)
 				-- test function pointer
 				local r, a = string.match(n, '(.-) %(%*%) (%b())')
+				if r and a then
+								-- only single arguments are supported
+								return 'function', r, string.match(a, '%(([^,]*)%))')
+				end
+				return nil
 end
 
 
@@ -71,6 +76,7 @@ local types_name = setmetatable({}, {
 								for n in iter do if space.byname and space.byname[n] then
 												space = space.byname[n]
 												if type(space)=='table' and space.label=='TypeAlias' then
+																print('^^^^^^^^', k)
 																local alias = space.xarg.type_name
 																if space.xarg.type_base~=alias then
 																				-- if it is not a pure object name, it should not have members
@@ -107,159 +113,12 @@ local types_name = setmetatable({}, {
 				end,
 })
 
---[[
-table.foreach(code.byname.hello.xarg, print)
-table.foreach(code.byname.hello[1].xarg, print)
-
-table.foreach(code.byname.world.xarg, print)
-table.foreach(code.byname.world[1].xarg, print)
-
-table.foreach(code.byname.string_type.xarg, print)
-table.foreach(code.byname.string_type[1].xarg, print)
---]]
-
-
-pushtype_table = {
-['void'] =   function(i) return '(void)(L, ' .. tostring(i) .. ')' end,
-['void*'] =   function(i) return 'lua_pushlightuserdata(L, ' .. tostring(i) .. ')' end,
-['void**'] = function(i) return 'lua_pushlightuserdata(L, ' .. tostring(i) .. ')' end,
-['void const*'] =   function(i) return 'lua_pushlightuserdata(L, ' .. tostring(i) .. ')' end,
-['void const**'] = function(i) return 'lua_pushlightuserdata(L, ' .. tostring(i) .. ')' end,
-
-['char*'] = function(i) return 'lua_pushstring(L, ' .. tostring(i) .. ')' end,
-['char**'] = function(i) return 'lqtL_pusharguments(L, ' .. tostring(i) .. ')' end,
-['char const*'] = function(i) return 'lua_pushstring(L, ' .. tostring(i) .. ')' end,
-['char const**'] = function(i) return 'lqtL_pusharguments(L, ' .. tostring(i) .. ')' end,
-
-['int'] =                    function(i) return 'lua_pushinteger(L, ' .. tostring(i) .. ')' end,
-['unsigned int'] =           function(i) return 'lua_pushinteger(L, ' .. tostring(i) .. ')' end,
-
-['short int'] =              function(i) return 'lua_pushinteger(L, ' .. tostring(i) .. ')' end,
-['unsigned short int'] =     function(i) return 'lua_pushinteger(L, ' .. tostring(i) .. ')' end,
-['short unsigned int'] =     function(i) return 'lua_pushinteger(L, ' .. tostring(i) .. ')' end,
-
-['long int'] =               function(i) return 'lua_pushinteger(L, ' .. tostring(i) .. ')' end,
-['unsigned long int'] =      function(i) return 'lua_pushinteger(L, ' .. tostring(i) .. ')' end,
-['long unsigned int'] =      function(i) return 'lua_pushinteger(L, ' .. tostring(i) .. ')' end,
-
-['long long int'] =          function(i) return 'lua_pushinteger(L, ' .. tostring(i) .. ')' end,
-['unsigned long long int'] = function(i) return 'lua_pushinteger(L, ' .. tostring(i) .. ')' end,
-
-['float'] =  function(i) return 'lua_pushnumber(L, ' .. tostring(i) .. ')' end,
-['double'] = function(i) return 'lua_pushnumber(L, ' .. tostring(i) .. ')' end,
-
-['bool'] = function(i) return 'lua_pushboolean(L, ' .. tostring(i) .. ')' end,
-}
-
---[[
-type_name = function(base, constant, volatile, reference, indirections)
-				print(base, constant, volatile, reference, indirections)
-				local ret = base
-				if not ret then return nil end
-				ret = ret .. (constant and ' const' or '')
-				ret = ret .. (volatile and ' volatile' or '')
-				ret = ret .. string.rep('*', indirections)
-				ret = ret .. (reference and '&' or '')
-				return ret
-end
---]]
-
-next_scope = function(path, i)
-				local ns, te, j = string.match(path, "([^:<>]+)(%b<>)()", i)
-				if not ns then ns, te, j = string.match(path, "([^:<>]+)()()", i) end
-				return ns, te, j
-end
-
-find_element = function(t, path, err)
-				local ns, te, i = next_scope(path)
-				while ns and ns~='' do
-								print ('===>', ns, '`'..te..'\'', i, path, t.xarg.name)
-								if type(te)=='number' then
-												if not t.byname[ns] then
-																print(path..' broken at '..ns..' -- '..tostring(err))
-																break
-												else
-																t = t.byname[ns]
-												end
-								else
-												local ot = t
-												for k, v in pairs(t.byname) do
-																if string.find(k, ns..'<')==1 then
-																				t = v
-																				break
-																end
-												end
-												if t==ot then print('broken template ' ..ns..te) end
-								end
-								t = solve_typedefs(t)
-								ns, te, i = next_scope(path, i)
-				end
-				return t
-end
-solve_typedefs = function(t)
-				if t.label=="TypeAlias" then
-								print('solving', t.xarg.name)
-								return find_element(t, t.xarg.type_base, "solve typedef")
-				else
-								return t
-				end
-end
-
-is = {
-				function_pointer = function(t)
-								return string.match(t.xarg.type_name, '%(%*%)')
-				end,
-				array = function(t)
-								return string.match(t.xarg.type_name, '%[%d+%]$')
-				end,
-				pointer = function(t)
-								return string.match(t.xarg.type_name, '%*$')
-				end,
-				reference = function(t)
-								return string.match(t.xarg.type_name, '%&$')
-				end,
-}
-push = {
-				function_pointer = function(t)
-								return function(x)
-												return 'lqt_pushpointer(L, '..tostring(x)..', "'..tostring(t.xarg.type_name)..'")'
-								end
-				end,
-				array = function(t)
-								return function(x) return '(array type) '..tostring(x) end
-				end,
-				pointer = function(t)
-								return function(x)
-												return 'lqt_pushpointer(L, &'..tostring(x)..', "'
-												..t.xarg.type_name:gsub(' const',''):gsub(' volatile','')..'")'
-								end
-				end,
-				reference = function(t)
-								return function(x)
-												return 'lqt_pushpointer(L, &'..tostring(x)..', "'
-												..t.xarg.type_name:gsub(' const',''):gsub(' volatile',''):gsub('%&$', '*')..'")'
-								end
-				end,
-}
-
-pushtype = function(t)
-				if type(t)~='table' or type(t.xarg)~='table' or type(t.xarg.type_name)~='string' then
-								-- TODO: throw error
-								return function(x) return '(not type) '..tostring(x) end
-				end
-				print('===>', find_element(code, t.xarg.type_base, t.xarg.id).label)
-				if pushtype_table[t.xarg.type_name] then return pushtype_table[t.xarg.type_name] end
-				-- TODO throw error
-				for i, p in pairs(is) do
-								if p(t) then return push[i](t) end
-				end
-				return function(x) return '=== UNKNOWN TYPE === '..(t.xarg.type_name)..' '..tostring(x) end
-end
-
 
 local cache = {}
 for _, v in pairs(xmlstream.byid) do
-				local __ = types_name[v.xarg.type_base or 'none']
+				if v.label~='TypeAlias' then
+								local __ = types_name[v.xarg.type_base or 'none']
+				end
 				--if v.xarg.scope~=v.xarg.context..'::' then print(v.label, v.xarg.id, v.xarg.type_name, v.xarg.scope, v.xarg.context) end
 				--cache[v.xarg.context] = true
 				--print(pushtype(v)(v.xarg.name), ' // '.._..': '..v.label..' : '..(v.xarg.type_name or ''))
