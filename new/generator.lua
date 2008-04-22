d92febd3e145f596bb103261e00765265e665a04
Mauro Iazzi <mauro.iazzi@gmail.com>
2008-04-23 00:27:54 +0200

    added some working code to manipulate functions

--- new/generator.lua
+++ new/generator.lua
@@ -56,13 +56,14 @@ local types_desc = setmetatable({}, {
 			return nil
 		else
 			t[k] = space
-			space.on_stack = 'userdata;'
+			--space.on_stack = 'userdata;' -- more precise info is needed
 		end
 		return t[k]
 	end,
 })
 
 local cache = {}
+--[[
 for _, v in pairs(xmlstream.byid) do
 	if v.xarg.type_base then
 		if not string.match(v.xarg.context, '[<,]%s*'..v.xarg.type_base..'%s*[>,]') then
@@ -71,24 +72,99 @@ for _, v in pairs(xmlstream.byid) do
 		end
 	end
 end
+--]]
+
+do
+	--local t = {}
+	--table.foreach(types_desc, function(i, j) t[j] = true end)
+	--table.foreach(types_desc, function(n,d) print(n, d.label, d.on_stack) end)
+end
 
 do
 	local t = {}
-	--table.foreach(types_desc, function(i, j) t[j] = true end)
-	table.foreach(types_desc, function(n,d) print(n, d.label, d.on_stack) end)
+	for _, v in pairs(xmlstream.byid) do
+		local o = t[v.xarg.fullname] or {}
+		table.insert(o, v)
+		t[v.xarg.fullname] = o
+	end
+	get_from_fullname = function(n)
+		return t[n]
+	end
+	--name_list = t
 end
 
-bind_function = function(f)
+
+type_on_stack = function(t)
+	local typename = type(t)=='string' and t or t.xarg.type_name
+	if rawget(base_types, typename) then
+		local ret = rawget(base_types, typename).on_stack
+		return ret
+	end
+	if type(t)=='string' or t.xarg.type_base==typename then
+		local identifier = get_from_fullname(typename)
+		assert(identifier and #identifier==1, 'cannot resolve base type: '..typename)
+		identifier = identifier[1]
+		if identifier.label=='Enum' then
+			return 'string;'
+		elseif identifier.label=='Class' then
+			return typename..'*;'
+		else
+			error('unknown identifier type: '..identifier.label)
+		end
+	else
+		if t.xarg.array then
+			error'I cannot manipulate arrays'
+		elseif string.match(typename, '%(%*%)') then
+			-- function pointer type
+			-- FIXME: the XML description does not contain this info
+			error'I cannot manipulate function pointers'
+		elseif t.xarg.indirections then
+			error'I cannot manipulate pointers'
+		else
+			-- this is any combination of constant, volatile and reference
+			-- we ignore this info and treat this as normal value
+			return type_on_stack(t.xarg.type_base)
+		end
+	end
+end
+
+-- TODO
+-- function_is_constructor
+-- function_is_destructor
+-- function_takes_this_pointer
+function_arguments_on_stack = function(f)
 	if type(f)~='table' or string.find(f.label, 'Function')~=1 then
 		error('this is NOT a function')
 	end
-	io.write(f.xarg.type_name .. ' ' .. f.xarg.fullname .. 
+	local args_on_stack = ''
+	--print('=====', f.xarg.fullname) 
+	for _,a in ipairs(f) do
+		local st, err = pcall(type_on_stack, a)
+		--if not st then table.foreach(a, print) end
+		assert(st, err)
+		args_on_stack = args_on_stack .. err
+	end
+	if f.xarg.member_of and not (f.xarg.static=='1') and f.xarg.member_of~='' then
+		args_on_stack = f.xarg.member_of .. '*;' .. args_on_stack
+	end
+	return args_on_stack
+end
+function_description = function(f)
+	if type(f)~='table' or string.find(f.label, 'Function')~=1 then
+		error('this is NOT a function')
+	end
+	local args_on_stack = function_arguments_on_stack(f)
+	return f.xarg.type_name .. ' ' .. f.xarg.fullname .. 
 	(f.xarg.static=='1' and ' [static]' or '')..
 	(f.xarg.virtual=='1' and ' [virtual]' or '')..
-	' [in ' .. tostring(f.xarg.member_of) .. ']\n')
+	' [in ' .. tostring(f.xarg.member_of) .. '] (' .. args_on_stack .. ')'
 end
 for _, v in pairs(xmlstream.byid) do
-	pcall(bind_function, v)
+	if string.find(v.label, 'Function')==1 then
+		local status, err = pcall(function_description, v)
+		io[status and 'stdout' or 'stderr']:write(err..'\n')
+	end
 end
+--table.foreach(name_list, print)
 
 
