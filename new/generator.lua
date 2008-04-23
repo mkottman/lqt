a8380fdb774179262a69c1818fe20cb57aacd40d
Mauro Iazzi <mauro.iazzi@gmail.com>
2008-04-23 13:24:58 +0200

    added code to distinguish constructor and destructor, return types and implicit arguments

--- new/generator.lua
+++ new/generator.lua
@@ -119,7 +119,15 @@ type_on_stack = function(t)
 			-- FIXME: the XML description does not contain this info
 			error'I cannot manipulate function pointers'
 		elseif t.xarg.indirections then
-			error'I cannot manipulate pointers'
+			if t.xarg.indirections=='1' then
+				local b = assert(get_from_fullname(t.xarg.type_base), 'unknown type base')[1]
+				if b.label=='Class' then
+					return t.xarg.type_base..'*;'
+				else
+					error('I cannot manipulate pointers to '..t.xarg.type_base)
+				end
+			end
+			error'I cannot manipulate double pointers'
 		else
 			-- this is any combination of constant, volatile and reference
 			-- we ignore this info and treat this as normal value
@@ -128,41 +136,73 @@ type_on_stack = function(t)
 	end
 end
 
--- TODO
--- function_is_constructor
--- function_is_destructor
--- function_takes_this_pointer
-function_arguments_on_stack = function(f)
+assert_function = function(f)
 	if type(f)~='table' or string.find(f.label, 'Function')~=1 then
 		error('this is NOT a function')
 	end
+end
+function_is_constructor = function(f)
+	assert_function(f)
+	return (f.xarg.member_of and f.xarg.member_of~=''
+	and f.xarg.fullname==(f.xarg.member_of..'::'..f.xarg.name) -- this should be always true
+	and string.match(f.xarg.member_of, f.xarg.name..'$')) and '[constructor]'
+end
+function_is_destructor = function(f)
+	assert_function(f)
+	return f.xarg.name:sub(1,1)=='~' and '[destructor]'
+end
+function_takes_this_pointer = function(f)
+	assert_function(f)
+	if f.xarg.member_of and not (f.xarg.static=='1') and f.xarg.member_of~=''
+		and not function_is_constructor(f) then
+		return f.xarg.member_of .. '*;'
+	end
+	return false
+end
+return_type = function(f)
+	assert_function(f)
+	if function_is_destructor(f) then
+		return nil
+	elseif function_is_constructor(f) then
+		-- FIXME: hack follows!
+		assert(f.xarg.type_name==f.xarg.type_base, 'return type of constructor is strange')
+		f.xarg.type_name = f.xarg.type_base..'*'
+		f.xarg.indirections='1'
+		return f
+	else
+		return f
+	end
+end
+function_arguments_on_stack = function(f)
+	assert_function(f)
 	local args_on_stack = ''
 	--print('=====', f.xarg.fullname) 
 	for _,a in ipairs(f) do
-		local st, err = pcall(type_on_stack, a)
+		--local st, err = pcall(type_on_stack, a)
 		--if not st then table.foreach(a, print) end
-		assert(st, err)
+		--assert(st, err)
+		local err = type_on_stack(a)
 		args_on_stack = args_on_stack .. err
 	end
-	if f.xarg.member_of and not (f.xarg.static=='1') and f.xarg.member_of~='' then
+	if function_takes_this_pointer(f) then
 		args_on_stack = f.xarg.member_of .. '*;' .. args_on_stack
 	end
 	return args_on_stack
 end
 function_description = function(f)
-	if type(f)~='table' or string.find(f.label, 'Function')~=1 then
-		error('this is NOT a function')
-	end
+	assert_function(f)
 	local args_on_stack = function_arguments_on_stack(f)
-	return f.xarg.type_name .. ' ' .. f.xarg.fullname .. 
+	return f.xarg.type_name .. ' ' .. f.xarg.fullname .. ' (' .. args_on_stack .. ')'..
 	(f.xarg.static=='1' and ' [static]' or '')..
 	(f.xarg.virtual=='1' and ' [virtual]' or '')..
-	' [in ' .. tostring(f.xarg.member_of) .. '] (' .. args_on_stack .. ')'
+	(function_is_constructor(f) and ' [constructor]' or '')..
+	(function_is_destructor(f) and ' [destructor]' or '')..
+	' [in ' .. tostring(f.xarg.member_of) .. ']'
 end
 for _, v in pairs(xmlstream.byid) do
 	if string.find(v.label, 'Function')==1 then
 		local status, err = pcall(function_description, v)
-		io[status and 'stdout' or 'stderr']:write(err..'\n')
+		io[status and 'stdout' or 'stderr']:write((status and '' or v.xarg.fullname..': ')..err..'\n')
 	end
 end
 --table.foreach(name_list, print)
