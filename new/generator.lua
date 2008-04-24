30d23a00541f95210e4068e97497008e302f00cd
Mauro Iazzi <mauro.iazzi@gmail.com>
2008-04-24 23:32:12 +0200

    added generic type_property function to help manipulating types

--- new/generator.lua
+++ new/generator.lua
@@ -79,6 +79,56 @@ type_on_stack = function(t)
 	end
 end
 
+local get_enum = function(fullname)
+	return function(i,j)
+		j = j or -i
+		return fullname .. ' arg' .. tostring(i) .. ' = static_cast< ' ..
+			fullname .. ' >(LqtGetEnumType(L, "' .. fullname .. '");'
+	end
+end
+
+type_properties = function(t)
+	local typename = type(t)=='string' and t or t.xarg.type_name
+	if rawget(base_types, typename) then
+		local ret = rawget(base_types, typename)
+		return ret.on_stack, ret.get
+	end
+	if type(t)=='string' or t.xarg.type_base==typename then
+		local identifier = get_from_fullname(typename)
+		assert(identifier and #identifier==1, 'cannot resolve base type: '..typename)
+		identifier = identifier[1]
+		if identifier.label=='Enum' then
+			return 'string;', get_enum(identifier.xarg.fullname)
+		elseif identifier.label=='Class' then
+			return typename..'*;'
+		else
+			error('unknown identifier type: '..identifier.label)
+		end
+	else
+		if t.xarg.array then
+			error'I cannot manipulate arrays'
+		elseif string.match(typename, '%(%*%)') then
+			-- function pointer type
+			-- FIXME: the XML description does not contain this info
+			error'I cannot manipulate function pointers'
+		elseif t.xarg.indirections then
+			if t.xarg.indirections=='1' then
+				local b = assert(get_from_fullname(t.xarg.type_base), 'unknown type base')[1]
+				if b.label=='Class' then
+					return t.xarg.type_base..'*;'
+				else
+					error('I cannot manipulate pointers to '..t.xarg.type_base)
+				end
+			end
+			error'I cannot manipulate double pointers'
+		else
+			-- this is any combination of constant, volatile and reference
+			-- we ignore this info and treat this as normal value
+			return type_on_stack(t.xarg.type_base)
+		end
+	end
+end
+
 assert_function = function(f)
 	if type(f)~='table' or string.find(f.label, 'Function')~=1 then
 		error('this is NOT a function')
@@ -135,6 +185,11 @@ end
 
 -- TODO: must wait for a way to specify pushing base types
 function_calling_code = function(f)
+	assert_function(f)
+	local ret, indent = '', '  '
+	for _,a in ipairs(f) do
+		ret = ret .. indent .. '\n'
+	end
 end
 
 function_description = function(f)
