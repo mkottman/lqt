d40012bca0b63df8a919ec723dd11d2afd4a5aeb
Mauro Iazzi <mauro.iazzi@gmail.com>
2008-04-30 01:10:50 +0200

    modularized base type descriptions

--- new/types.lua
+++ new/types.lua
@@ -1,15 +1,17 @@
+local base_types = (...)
+
 local BaseType = function(s)
 	s = tostring(s)
 	return {
-		label = "BaseType",
-		xargs = {  },
-		on_stack = s..';',
+		--label = "BaseType",
+		--xargs = {  },
+		desc = s..';',
 		get = function(i, j)
 			j = j or -i
-			return 'LqtGetBaseType_'..s..'(L, '..tostring(j)..');'
+			return 'lua_to'..s..'(L, '..tostring(j)..');'
 		end,
 		push = function(j) -- must handle arguments (e.g. in virtual callbacks) and return values
-			return 'LqtPushBaseType_'..s..'(L, '..tostring(j)..');'
+			return 'lua_push'..s..'(L, '..tostring(j)..');'
 		end,
 		test = function(i, j)
 			error'not implemented' -- TODO
@@ -19,43 +21,24 @@ local BaseType = function(s)
 	}
 end
 
-local base_types = {
-	--['void'] =   function(i) return '(void)(L, ' .. tostring(i) .. ')' end,
-	--['void*'] =   function(i) return 'lua_pushlightuserdata(L, ' .. tostring(i) .. ')' end,
-	--['void**'] = function(i) return 'lua_pushlightuserdata(L, ' .. tostring(i) .. ')' end,
-	--['void const*'] =   function(i) return 'lua_pushlightuserdata(L, ' .. tostring(i) .. ')' end,
-	--['void const**'] = function(i) return 'lua_pushlightuserdata(L, ' .. tostring(i) .. ')' end,
+base_types['char const*'] = BaseType'string'
+base_types['int'] = BaseType'integer'
+base_types['unsigned int'] = BaseType'integer'
+base_types['short'] = BaseType'integer'
+base_types['short int'] = BaseType'integer'
+base_types['unsigned short'] = BaseType'integer'
+base_types['unsigned short int'] = BaseType'integer'
+base_types['short unsigned int'] = BaseType'integer'
+base_types['long'] = BaseType'integer'
+base_types['unsigned long'] = BaseType'integer'
+base_types['long int'] = BaseType'integer'
+base_types['unsigned long int'] = BaseType'integer'
+base_types['long unsigned int'] = BaseType'integer'
+base_types['long long'] = BaseType'integer'
+base_types['unsigned long long'] = BaseType'integer'
+base_types['long long int'] = BaseType'integer'
+base_types['unsigned long long int'] = BaseType'integer'
+base_types['float'] = BaseType'number'
+base_types['double'] = BaseType'number'
+base_types['bool'] = BaseType'boolean'
 
-	--['char'] = BaseType'string',
-	--['char*'] = BaseType'string',
-	--['char**'] = function(i) return 'lqtL_pusharguments(L, ' .. tostring(i) .. ')' end,
-	['char const*'] = BaseType'string',
-	--['char const**'] = function(i) return 'lqtL_pusharguments(L, ' .. tostring(i) .. ')' end,
-
-	['int'] =                    BaseType'integer',
-	['unsigned int'] =           BaseType'integer',
-
-	['short'] =                  BaseType'integer',
-	['short int'] =              BaseType'integer',
-	['unsigned short'] =         BaseType'integer',
-	['unsigned short int'] =     BaseType'integer',
-	['short unsigned int'] =     BaseType'integer',
-
-	['long'] =                   BaseType'integer',
-	['unsigned long'] =          BaseType'integer',
-	['long int'] =               BaseType'integer',
-	['unsigned long int'] =      BaseType'integer',
-	['long unsigned int'] =      BaseType'integer',
-
-	['long long'] =              BaseType'integer',
-	['unsigned long long'] =     BaseType'integer',
-	['long long int'] =          BaseType'integer',
-	['unsigned long long int'] = BaseType'integer',
-
-	['float'] =  BaseType'number',
-	['double'] = BaseType'number',
-
-	['bool'] = BaseType'bool',
-}
-
-return base_types
