1cf37f1c32166b880971d056edd0c96b21a8ef71
Mauro Iazzi <mauro.iazzi@gmail.com>
2008-05-19 19:30:00 +0200

    changed the way types.lua returns the typesystem table

--- new/types.lua
+++ new/types.lua
@@ -1,4 +1,4 @@
-local base_types = (...)
+local base_types = (...) or {}
 
 local BaseType = function(s)
 	s = tostring(s)
@@ -7,20 +7,17 @@ local BaseType = function(s)
 		--xargs = {  },
 		desc = s..';',
 		get = function(j)
-			return 'lua_to'..s..'(L, '..tostring(j)..');'
+			return 'lua_to'..s..'(L, '..tostring(j)..');', 1
 		end,
 		push = function(j) -- must handle arguments (e.g. in virtual callbacks) and return values
-			return 'lua_push'..s..'(L, '..tostring(j)..');'
+			return 'lua_push'..s..'(L, '..tostring(j)..');', 1
 		end,
-		test = function(j)
-			error'not implemented' -- TODO
-			return 'LqtTestBaseType_'..s..'(L, '..tostring(j)..')'
-		end,
-		num = 1,
 	}
 end
 
 base_types['char const*'] = BaseType'string'
+base_types['char'] = BaseType'integer'
+base_types['unsigned char'] = BaseType'integer'
 base_types['int'] = BaseType'integer'
 base_types['unsigned int'] = BaseType'integer'
 base_types['short'] = BaseType'integer'
@@ -41,3 +38,4 @@ base_types['float'] = BaseType'number'
 base_types['double'] = BaseType'number'
 base_types['bool'] = BaseType'boolean'
 
+return base_types
