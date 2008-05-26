5ffa39c931dcb9dec2ff7f85dcd97c6ac09738be
Mauro Iazzi <mauro.iazzi@gmail.com>
2008-05-26 19:30:33 +0200

    added types necessary for creating QApplications

--- generator/types.lua
+++ generator/types.lua
@@ -118,6 +118,32 @@ base_types['float'] = number_type
 base_types['double'] = number_type
 base_types['bool'] = bool_type
 
+---[[
+base_types['int&'] = {
+	get = function(j)
+		return '*lqtL_tointref(L, '..j..')', 1
+	end,
+	push = function(j) -- must handle arguments (e.g. in virtual callbacks) and return values
+		return 'lua_pushinteger(L, '..tostring(j)..')', 1
+	end,
+	test = function(j) -- must handle arguments (e.g. in virtual callbacks) and return values
+		return 'lqtL_isinteger(L, '..tostring(j)..')', 1
+	end,
+}
+base_types['char**'] = {
+	get = function(j)
+		return 'lqtL_toarguments(L, '..tostring(j)..')', 1
+	end,
+	push = function(j) -- must handle arguments (e.g. in virtual callbacks) and return values
+		return 'lqtL_pusharguments(L, '..tostring(j)..')', 1
+	end,
+	test = function(j) -- must handle arguments (e.g. in virtual callbacks) and return values
+		return 'lua_istable(L, '..tostring(j)..')', 1
+	end,
+}
+--]]
+
+
 base_types['QSizeF'] = {
 	get = function(i) return 'QSizeF(lua_tonumber(L, '..i..'), lua_tonumber(L, '..i..'+1))', 2 end,
 	push = function(i) return 'lua_pushnumber(L, '..i..'.width()), lua_pushnumber(L, '..i..'.height())', 2 end,
