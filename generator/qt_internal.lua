237a2dbcd36db5cd4bb2b4d33ee2c7f770b4d328
Mauro Iazzi <mauro.iazzi@gmail.com>
2008-07-08 20:26:44 +0200

    added QtConcurent class to list of ignored classes

--- generator/qt_internal.lua
+++ generator/qt_internal.lua
@@ -6,6 +6,9 @@ for c in pairs(classes) do
 	local n = c.xarg.name
 	if n~=string.lower(n) and not (string.match(n, '_')
 			or c.xarg.fullname=='QAtomic'
+			or c.xarg.fullname=='QAtomicInt'
+			or c.xarg.fullname=='QBasicAtomic'
+			or c.xarg.fullname=='QBasicAtomicInt'
 			or c.xarg.fullname=='QDebug::Stream'
 			or c.xarg.fullname=='QForeachContainerBase'
 			or c.xarg.fullname=='QByteArray::Data'
@@ -19,6 +22,13 @@ for c in pairs(classes) do
 			or c.xarg.fullname=='QWindowsXPStyle'
 			or c.xarg.fullname=='QWindowsVistaStyle'
 			or c.xarg.fullname=='QMacStyle'
+			or c.xarg.fullname=='QtConcurrent::internal::ExceptionStore'
+			or c.xarg.fullname=='QtConcurrent::BlockSizeManager'
+			or c.xarg.fullname=='QtConcurrent::ResultItem'
+			or c.xarg.fullname=='QtConcurrent::ResultIteratorBase'
+			or c.xarg.fullname=='QtConcurrent::ResultStoreBase'
+			or c.xarg.fullname=='QtConcurrent::ThreadEngineBase'
+			or c.xarg.fullname=='QtConcurrent::ThreadEngineSemaphore'
 			or c.xarg.fullname=='QThreadStorageData') then
 		ret1[c] = true
 	end
