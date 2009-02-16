d745b3c498ddb935388afa1f3c8255b482c10be1
Mauro Iazzi <mauro.iazzi@gmail.com>
2009-02-16 08:45:34 +0100

    properly handle namespaces in base classes list

--- generator/generator.lua
+++ generator/generator.lua
@@ -752,7 +752,7 @@ local print_metatable = function(c)
 	for b in string.gmatch(c.xarg.bases_with_attributes or '', '([^;]*);') do
 		if not string.match(b, '^virtual') then
 			b = string.gsub(b, '^[^%s]* ', '')
-			bases = bases .. '{"' .. b .. '*", (char*)(void*)static_cast<'..b..'*>(('..c.xarg.fullname..'*)1)-(char*)1},\n'
+			bases = bases .. '{"'..string.gsub(b,'::','.')..'*", (char*)(void*)static_cast<'..b..'*>(('..c.xarg.fullname..'*)1)-(char*)1},\n'
 		end
 	end
 	bases = 'static lqt_Base lqt_base'..c.xarg.id..'[] = { '..bases..'{NULL, 0} };\n'
