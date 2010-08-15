4a339d1c9fd27cb87240fb11d0ed3366c09b15b3
Michal Kottman <michal.kottman@gmail.com>
2010-08-15 19:19:28 +0200

    fix binding of overloaded operators

--- generator/operators.lua
+++ generator/operators.lua
@@ -17,24 +17,31 @@ local operatorTrans = {
 	['=='] = '__eq',
 }
 
+local function is_helper_function(name)
+	local s1 = name:sub(1,1)
+	local s2 = name:sub(2,2)
+	return s1 == 'q' and s2 == s2:upper()
+end
+
 function fix_operators(index)
 	for f in pairs(index) do
 		if f.label == "Function" then
-			if f.xarg.name == 'operator-' and #f.arguments == 1 then
-				-- use '_' as a marker for unary minus
-				f.xarg.name = 'operator_'
-				table.remove(f, 1)
-				table.remove(f.arguments, 1)				
-			elseif f.xarg.name:match("^operator") and f.xarg.friend then
+			if f.xarg.name:match("^operator") and f.xarg.friend then
 				if f[1].xarg.type_base == f.xarg.member_of then
 					-- overloaded friend operator - its first argument is 'this', 
 					-- needs to be removed
 					table.remove(f, 1)
 					table.remove(f.arguments, 1)
+					if f.xarg.name == 'operator-' and #f.arguments == 0 then
+						-- use '_' as a marker for unary minus
+						f.xarg.name = 'operator_'
+					end
 				else
 					-- operator in form: number OP class - do not bind these
 					f.ignore = true
 				end
+			elseif is_helper_function(f.xarg.name) then
+				f.ignore = true
 			end
 		end
 	end
